8e563857ce59f8b7d495ff32daf54bb735aa071f
KostaNau <naumov.k@gmail.com>
2017-06-15 22:31:02 +0300

    Inital commit

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+## Description
+The script check up domain expiration date and http response code for each site from user list.
+
+
+## How to use
+Run the script ```python3 check_site_healty.py``` with postional argument ```path to the user's site list```
+
+**User's site list requirements** *Each line must contain only one url.*
+
+
+### Examples
+```python3 check_site_healty.py path/to/site list```
+
+**Output**
+```
+==========================================================
+Domain: http://your-domain.ru
+HTTP (Code 200): True
+Expiration date (at least 30 days): True
+==========================================================
+Domain: http://www.somedomain.com
+HTTP (Code 200): True
+Expiration date (at least 30 days): True
+==========================================================
+Domain: http://foo.bar
+HTTP (Code 200): Failed to establish a new connection: unknown url!!!
+Expiration date (at least 30 days): Failed to determine expiration date. Unknown url!!!
+==========================================================
+```
+
+### Requriments
+Install the dependencies from requirements.txt using pip:
+
+```pip install -r requirements.txt```
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,64 @@
+import re
+import argparse
+from datetime import timedelta, datetime
+
+import whois
+import requests
+
+
+def parse() -> bytes:
+    parser = argparse.ArgumentParser(
+        description='''Simple site monitoring script.
+        Check http status code (200 is OK) and expiration date
+        (no least than 30 days)''')
+    parser.add_argument('target',
+                        type=str,
+                        help="path to the url list (txt)")
+    args = parser.parse_args()
+    return args.target
+
+
+def adapt_urls(urls):
+    http = re.compile(r"^(http|https)://")
+    transform = lambda url: "http://{}".format(url)
+    return [url if http.match(url) else transform(url) for url in urls]
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as opened_file:
+        return [url.strip() for url in opened_file]
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        http_ok = 200
+        return requests.get(url).status_code == http_ok
+    except requests.exceptions.ConnectionError:
+        return "Failed to establish a new connection: unknown url!!!"
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    try:
+        domain = whois.whois(url)
+        date_delta = timedelta(days=30)
+        domain_date = domain.expiration_date
+        return (domain_date - date_delta) > datetime.now()
+    except TypeError:
+        return "Failed to determine expiration date. Unknown url!!!"
+
+
+def main():
+    url_list = parse()
+    raw_urls = load_urls4check(url_list)
+    urls = adapt_urls(raw_urls)
+    for url in urls:
+        status_code = is_server_respond_with_200(url)
+        expr_date = get_domain_expiration_date(url)
+        print("Domain: {}".format(url))
+        print("HTTP (Code 200): {}".format(status_code))
+        print("Expiration date (at least 30 days): {}".format(expr_date))
+        print('==========================================================')
+
 
 if __name__ == '__main__':
-    pass
+    main()
